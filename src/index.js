eab271b265edbde98cff9758ac58824be4c6b075
viviramji <wrjr04@gmail.com>
2018-11-03 16:30:59 -0500

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,92 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Card = (props) => {
+  return (
+    <div style={{margin: '1em'}}>
+      <img alt="Profile" width="75" src={props.avatar_url} />
+      <div style={{display: 'inline-block', marginLeft: 10}}>
+        <div style={{fontSize: '1.25em', fontWeight: 'bold'}}> 
+        	{props.name}
+        </div>
+        <div>{props.company}</div>
+      </div>
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const CardList = (props) => {
+	return (
+  //Props nameProp = Value {...Card} spred operator
+  	<div> 
+      	{props.cards.map(card => <Card key={card.id} {...card}/>)}
+  	</div>
+  );
+}
+
+class Form extends React.Component {
+	
+  state = {
+  	userName: ''
+  };
+	
+  handleSubmit = (e) => {
+  	e.preventDefault();
+    //console.log('Event: from subit', this.state.userName);
+    //ajax ... (fetch or axios)    
+    axios.get(`https://api.github.com/users/${this.state.userName}`)
+    	.then( resp => {
+      	this.props.onSubmit(resp.data)
+        this.setState({userName: ''})
+      });
+    
+  };
+  
+	render() {
+  	return(
+    	<form onSubmit={this.handleSubmit}>
+    	  <input 
+        	type="text"
+          //ref = {(input) => this.userNameInput = input}
+          value={this.state.userName}
+          onChange={(e) => this.setState({userName: e.target.value})}
+          
+        	placeholder="Github username" required />
+        <button type="submit">Add card</button>
+    	</form>
+    );
+  }
+}
+
+class App extends React.Component {
+	state = {
+  	cards: [
+    ]
+  };
+  
+  addNewCard = (cardInfo) => {
+  	this.setState(prevState => ({
+    	cards: prevState.cards.concat(cardInfo)
+    }))
+  };
+  
+	render(){
+  	return(
+    	<div>
+  	  	<Form onSubmit={this.addNewCard} />
+      	<CardList cards={this.state.cards} />
+  		</div>
+    );  	
+  }
+}
+
+
+// ========================================
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
